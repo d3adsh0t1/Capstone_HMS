d9318056597f6080298e56c4c14fc0acf47fac2c
Yash Agarwal <yash.a@innovaccer.com>
2022-04-28 12:45:07 +0530

    first commit

---
+++ hospital/views.py
@@ -0,0 +1,74 @@
+from django.shortcuts import render,redirect,reverse
+from . import forms,models
+from django.contrib.auth.models import Group
+from django.http import HttpResponseRedirect
+from django.contrib.auth.decorators import login_required,user_passes_test
+from django.conf import settings
+
+# Create your views here.
+def home_view(request):
+    if request.user.is_authenticated:
+        return HttpResponseRedirect('afterlogin')
+    return render(request,'hospital/index.html')
+
+
+def adminclick_view(request):
+    if request.user.is_authenticated:
+        return HttpResponseRedirect('afterlogin')
+    return render(request,'hospital/adminclick.html')
+
+def admin_signup_view(request):
+    form=forms.AdminSigupForm()
+    if request.method=='POST':
+        form=forms.AdminSigupForm(request.POST)
+        if form.is_valid():
+            user=form.save()
+            user.set_password(user.password)
+            user.save()
+            my_admin_group = Group.objects.get_or_create(name='ADMIN')
+            my_admin_group[0].user_set.add(user)
+            return HttpResponseRedirect('adminlogin')
+    return render(request,'hospital/adminsignup.html',{'form':form})
+
+
+def is_admin(user):
+    return user.groups.filter(name='ADMIN').exists()
+
+
+
+#---------AFTER ENTERING CREDENTIALS WE CHECK WHETHER USERNAME AND PASSWORD IS OF ADMIN,DOCTOR OR PATIENT
+def afterlogin_view(request):
+    if is_admin(request.user):
+        return redirect('admin-dashboard')
+
+
+# #---------------------------------------------------------------------------------
+# #------------------------ ADMIN RELATED VIEWS START ------------------------------
+# #---------------------------------------------------------------------------------
+@login_required(login_url='adminlogin')
+@user_passes_test(is_admin)
+def admin_dashboard_view(request):
+    #for both table in admin dashboard
+    doctors=models.Doctor.objects.all().order_by('-id')
+    patients=models.Patient.objects.all().order_by('-id')
+    #for three cards
+    doctorcount=models.Doctor.objects.all().filter(status=True).count()
+    pendingdoctorcount=models.Doctor.objects.all().filter(status=False).count()
+
+    patientcount=models.Patient.objects.all().filter(status=True).count()
+    pendingpatientcount=models.Patient.objects.all().filter(status=False).count()
+
+    appointmentcount=models.Appointment.objects.all().filter(status=True).count()
+    pendingappointmentcount=models.Appointment.objects.all().filter(status=False).count()
+    mydict={
+    'doctors':doctors,
+    'patients':patients,
+    'doctorcount':doctorcount,
+    'pendingdoctorcount':pendingdoctorcount,
+    'patientcount':patientcount,
+    'pendingpatientcount':pendingpatientcount,
+    'appointmentcount':appointmentcount,
+    'pendingappointmentcount':pendingappointmentcount,
+    }
+    return render(request,'hospital/admin_dashboard.html',context=mydict)
+
---
+++ hospitalmanagement/urls.py
@@ -0,0 +1,31 @@
+from django.contrib import admin
+from django.urls import path
+from hospital import views
+from django.contrib.auth.views import LoginView,LogoutView
+
+
+#-------------FOR ADMIN RELATED URLS
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('',views.home_view,name=''),
+
+
+    # path('aboutus', views.aboutus_view),
+    # path('contactus', views.contactus_view),
+
+
+    path('adminclick', views.adminclick_view),
+
+    path('adminsignup', views.admin_signup_view),
+    
+    path('adminlogin', LoginView.as_view(template_name='hospital/adminlogin.html')),
+
+
+
+    path('afterlogin', views.afterlogin_view,name='afterlogin'),
+    path('logout', LogoutView.as_view(template_name='hospital/index.html'),name='logout'),
+
+
+    path('admin-dashboard', views.admin_dashboard_view,name='admin-dashboard'),
+
+]
