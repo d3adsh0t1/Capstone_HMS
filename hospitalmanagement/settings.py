0f1a03284489f55ebfc5e5ea4a91f6f6a9f84f37
Aalind465 <aalind.sharma@innovaccer.com>
2022-05-03 20:02:31 +0530

    add

--- hospitalmanagement/settings.py
+++ hospitalmanagement/settings.py
@@ -27,9 +27,7 @@ SECRET_KEY = 'hpbv()ep00boce&o0w7z1h)st148(*m@6@-rk$nn)(n9ojj4c0'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [
-'localhost',
-'127.0.0.1']
+ALLOWED_HOSTS = ['hms-dep1.herokuapp.com','127.0.0.1']
 
 
 # Application definition
