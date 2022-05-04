efd3e910b6cc61a59a174c02b856cc8b8a31426b
Aalind465 <aalind.sharma@innovaccer.com>
2022-05-04 10:11:32 +0530

    add

--- hospitalmanagement/settings.py
+++ hospitalmanagement/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
-
+import django_heroku
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 TEMPLATE_DIR = os.path.join(BASE_DIR,'templates')
@@ -134,3 +134,5 @@ STATIC_ROOT=os.path.join(BASE_DIR,'STATICFILES_DIRS')
 
 LOGIN_REDIRECT_URL='/afterlogin'
 
+# Activate Django-Heroku.
+django_heroku.settings(locals())
